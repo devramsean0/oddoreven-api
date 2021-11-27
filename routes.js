8a625262863a9cabad70812b74fa48bf7d1d0c04
Outramsean0 <81807361+devramsean0@users.noreply.github.com>
2021-11-27 10:10:00 +0000

    small docs

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 function routes(app) {
     app.get('/', function (request, reply) {
-        reply.send("Our first route")
+        reply.send(`Docs: \n is it odd or even?: https://oddoreven.sean-outram.dev/oddoreven/{number} \n odd?: https://oddoreven.sean-outram.dev/odd/{number} \n even?:https://oddoreveb.sean-outram.dev/even/{number}`)
     })
 }
 module.exports = routes;
