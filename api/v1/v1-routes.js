893f2bed33629de0266852dcfdc0a6427ad0231d
Outramsean0 <81807361+devramsean0@users.noreply.github.com>
2021-11-27 12:12:57 +0000

    added json routes

--- api/v1/v1-routes.js
+++ api/v1/v1-routes.js
@@ -26,5 +26,32 @@ function v1routes(app) {
             reply.send('False')
         }
     })
+    // odd or even? JSON
+    app.get('/v1/oddoreven/:number/json', function (request, reply) {
+        var number = request.params.number
+        if (number % 2 === 0) {
+            reply.send({ response: 'Even'})
+        } else {
+            reply.send({ response: 'Odd'})    
+        }
+    })
+    // odd? JSON
+    app.get('/v1/odd/:number/json', function (request, reply) {
+        var number = request.params.number
+        if (number % 2 === 0) {
+            reply.send({ response: 'False'})
+        } else {
+            reply.send({ response: 'True'})
+        }
+    })
+    // even? JSON
+    app.get('/v1/even/:number/json', function (request, reply) {
+        var number = request.params.number
+        if (number % 2 === 0) {
+            reply.send({ response: 'True'})
+        } else {
+            reply.send({ response: 'False'})
+        }
+    })
 }
 module.exports = v1routes
